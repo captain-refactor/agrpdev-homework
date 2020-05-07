b070bea20aa30a0bc06eeb7b34cab462944dfc08
Jan Kremen <kremenh@gmail.com>
2020-05-07 15:47:29 +0200

    create reusable AppButton component

---
+++ src/components/AppButton.tsx
@@ -0,0 +1,14 @@
+import Button from "@material-ui/core/Button";
+import React from "react";
+import styled from "@material-ui/core/styles/styled";
+
+export const AppButton = styled(Button)({
+  color: "#fa4f7c",
+  "&.active": {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    color: "white",
+  },
+  "&:focus": {
+    outline: "solid 1px #FF8E53",
+  },
+});
